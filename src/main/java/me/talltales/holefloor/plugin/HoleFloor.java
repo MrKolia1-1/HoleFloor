d95e2ee0d61de6b60047cbca1fa528b0df6387b1
MrKolia1_1 <kolya-bobrov-2017@mail.ru>
2022-07-01 17:30:47 +0300

    Default commit

--- src/main/java/me/talltales/holefloor/plugin/HoleFloor.java
+++ src/main/java/me/talltales/holefloor/plugin/HoleFloor.java
@@ -1,6 +1,19 @@
 package me.talltales.holefloor.plugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPluginLoader;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.Logger;
 
 public class HoleFloor extends JavaPlugin {
     public HoleFloor() {
@@ -20,5 +33,84 @@ public class HoleFloor extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
+    }
+
+    protected HoleFloor(JavaPluginLoader loader, PluginDescriptionFile description, File dataFolder, File file) {
+        super(loader, description, dataFolder, file);
+    }
+
+    @Override
+    protected File getFile() {
+        return super.getFile();
+    }
+
+    @Override
+    public FileConfiguration getConfig() {
+        return super.getConfig();
+    }
+
+    @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+    }
+
+    @Override
+    public void saveConfig() {
+        super.saveConfig();
+    }
+
+    @Override
+    public void saveDefaultConfig() {
+        super.saveDefaultConfig();
+    }
+
+    @Override
+    public void saveResource(String resourcePath, boolean replace) {
+        super.saveResource(resourcePath, replace);
+    }
+
+
+    @Override
+    public InputStream getResource(String filename) {
+        return super.getResource(filename);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return super.onCommand(sender, command, label, args);
+    }
+
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return super.onTabComplete(sender, command, alias, args);
+    }
+
+
+    @Override
+    public PluginCommand getCommand(String name) {
+        return super.getCommand(name);
+    }
+
+
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return super.getDefaultWorldGenerator(worldName, id);
+    }
+
+
+    @Override
+    public BiomeProvider getDefaultBiomeProvider(String worldName, String id) {
+        return super.getDefaultBiomeProvider(worldName, id);
+    }
+
+    @Override
+    public Logger getLogger() {
+        return super.getLogger();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
